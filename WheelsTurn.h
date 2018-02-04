18e9037957ab87bc347c788af05f3c5d6b8b6e81
ethantrokie <ethantrokie@gmail.com>
2018-02-04 11:07:32 -0600

    wheel controls

---
+++ WheelsTurn.h
@@ -0,0 +1,8 @@
+#include <SoftwareSerial.h>
+
+void goStraight(int in1a,int in2a, int in1b,int in2b);
+void turnLeft(int in1a,int in2a, int in1b,int in2b);
+void turnRight(int in1a,int in2a, int in1b,int in2b);
+void goBackward(int in1a,int in2a, int in1b,int in2b);
+void doBreak(int in1a,int in2a, int in1b,int in2b);
+void doCoast(int in1a,int in2a, int in1b,int in2b);
