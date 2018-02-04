321f1dace899071a04af7b07693f04b4e9337b89
Josh Werblin <joshwerblin@gmail.com>
2018-02-04 11:34:23 -0600

    Ethan doesn't know the difference bt break and brake

---
+++ DRV8838.h
@@ -0,0 +1,6 @@
+#include <SoftwareSerial.h>
+
+void drive(int phase,int enable,int nsleep);
+void reverse(int phase,int enable,int nsleep);
+void brake(int phase,int enable,int nsleep);
+void coast(int phase,int enable,int nsleep);
--- WheelsTurn.h
+++ WheelsTurn.h
@@ -4,5 +4,5 @@ void goStraight(int in1a,int in2a, int in1b,int in2b);
 void turnLeft(int in1a,int in2a, int in1b,int in2b);
 void turnRight(int in1a,int in2a, int in1b,int in2b);
 void goBackward(int in1a,int in2a, int in1b,int in2b);
-void doBreak(int in1a,int in2a, int in1b,int in2b);
+void doBrake(int in1a,int in2a, int in1b,int in2b);
 void doCoast(int in1a,int in2a, int in1b,int in2b);
