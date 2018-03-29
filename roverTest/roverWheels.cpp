7efcde86cd2165a480dc96c43b9e7a7a13be4393
Yichen <ichisaur@gmail.com>
2018-03-28 21:50:21 -0500

    Reduce Rover Speed

--- roverTest/roverWheels.cpp
+++ roverTest/roverWheels.cpp
@@ -3,16 +3,16 @@
 
 void forwards(int leftA, int leftB, int rightA, int rightB, int diff) {
   if (diff >= 0) {
-    analogWrite(leftA, 255);
+    analogWrite(leftA, 0.5*(255));
     analogWrite(leftB, 0);
-    analogWrite(rightA, max(255-diff, 0));
-    analogWrite(rightB, min(255-diff, 0));
+    analogWrite(rightA, max(0.5*(255-diff), 0));
+    analogWrite(rightB, min(0.5*(255-diff), 0));
   }
   else {
-    analogWrite(rightA, 255);
+    analogWrite(rightA, 0.5*255);
     analogWrite(rightB, 0);
-    analogWrite(leftA, max(255+diff, 0));
-    analogWrite(leftB, min(255+diff, 0));
+    analogWrite(leftA, max(0.5*(255+diff), 0));
+    analogWrite(leftB, min(0.5*(255+diff), 0));
   }
 }
 
@@ -22,15 +22,15 @@ void backwards(int leftA, int leftB, int rightA, int rightB, int diff){
 
   if (diff >= 0) {
     analogWrite(leftA, 0);
-    analogWrite(leftB, 255);
-    analogWrite(rightA, min(255-diff, 0));
-    analogWrite(rightB, max(255-diff, 0));
+    analogWrite(leftB, 0.5*255);
+    analogWrite(rightA, min(0.5*(255-diff), 0));
+    analogWrite(rightB, max(0.5*(255-diff), 0));
   }
   else {
     analogWrite(rightA, 0);
-    analogWrite(rightB, 255);
-    analogWrite(leftA, min(255+diff, 0));
-    analogWrite(leftB, max(255+diff, 0));
+    analogWrite(rightB, 0.5*(255));
+    analogWrite(leftA, min(0.5*(255+diff), 0));
+    analogWrite(leftB, max(0.5*(255+diff), 0));
   }
   
 
